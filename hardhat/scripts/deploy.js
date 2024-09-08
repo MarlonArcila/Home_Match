e1d0e21ca64282833b3c4c5fee707cef292640a9
MarlonArcila <155872432+MarlonArcila@users.noreply.github.com>
2024-09-08 13:07:30 -0500

    Create deploy.js

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+	
+  const contractName = "Pujas"; 
+  const contract = await hre.ethers.deployContract(contractName);
+
+  await contract.waitForDeployment();
+
+  console.log(`Contract Deployed to address ${await contract.getAddress()}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
